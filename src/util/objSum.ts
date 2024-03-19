a6145a3a09a5af5cfd8822298ef16b018402057e
liruifengv <liruifeng1024@gmail.com>
2024-03-19 14:06:30 +0800

    Add share to X

--- src/util/objSum.ts
+++
@@ -1,3 +0,0 @@
-export function objSum(obj: Record<string, number>): number {
-  return Object.values(obj).reduce((sum, i) => sum + i, 0);
-}
--- src/util/resizedGitHubAvatarURL.ts
+++
@@ -1,5 +0,0 @@
-export const resizedGitHubAvatarURL = (avatarURL: string, size: number) => {
-  const url = new URL(avatarURL);
-  url.searchParams.set('s', String(size));
-  return url.href;
-};
